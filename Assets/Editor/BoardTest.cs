29bd9829fa7639354c71ca326786199ec5305c0d
Juan Mora <juan.mora@brainz.co>
2014-07-31 18:44:22 -0500

    First commit

---
+++ Assets/Editor/BoardTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+[TestFixture]
+public class BoardTest
+{
+	[Test]
+	public void InitialPositionIsOne()
+	{
+		Board board = new Board();
+		Assert.AreEqual(1, board.Position);
+	}
+
+	[Test]
+	public void PositionIsFourAfterMovingThreeSpaces()
+	{
+		Board board = new Board();
+		board.Move (3);
+		Assert.AreEqual(4, board.Position);
+	}
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board
+{
+	public int Position
+	{
+		get;
+		private set;
+	}
+
+	public Board()
+	{
+		Position = 1;
+	}
+
+	public void Move(int spaces)
+	{
+		Position = 4;
+	}
+}
